81d1456f21e4de23f01fdec62c825349c7288b5c
Prakhar <prakhar@isglobalweb.com>
2019-03-28 12:11:56 +0530

    show functionality

--- assets/js/text-editor.js
+++ assets/js/text-editor.js
@@ -6,7 +6,7 @@ class TxtEditor {
     createBtn(text) {
         let btn =  document.createElement("BUTTON");
         btn.append(document.createTextNode(text));
-        btn.setAttribute("btn-id",text)
+        btn.setAttribute("btn-id",text.toLowerCase());
         return btn;
 
         
@@ -18,10 +18,15 @@ class TxtEditor {
     initTextEditor(val) {
 
         var allEdtr = document.querySelectorAll(val);
+        
         let counter = 0;
+
         allEdtr.forEach(element => {
 
             element.style.display = "none";
+            let createOutputBox = document.createElement("DIV");
+            createOutputBox.setAttribute("output-id",counter);
+
 
             let createMainEdtr = document.createElement("DIV");
             
@@ -32,36 +37,105 @@ class TxtEditor {
             let createBody = document.createElement("DIV");
             let createFooter = document.createElement("DIV");
             
-            createMainEdtr.classList.add("editor-main")
+            createMainEdtr.classList.add("editor-main");
             createHeader.classList.add("editor-header");
             createBody.classList.add("editor-body");
             createFooter.classList.add("editor-footer");
             
             createBody.append(this.createInput());
 
+            createFooter.append(this.createBtn("Show"));
+            
+            createFooter.childNodes.forEach(elem => {
+               
+                console.log(elem,elem.getAttribute("btn-id"));
+                
+                elem.addEventListener("click",evntApply);
+                
+                function evntApply(event) {
+                    
+                    var btnVal = event.target.getAttribute("btn-id");
+
+                    console.log("Switch value get "+ btnVal);
+
+                    switch(btnVal) {
+
+                        case "show":
+
+                              if( createBody.hasChildNodes()) {
+                            
+                                let bodyTextLength = createBody.childNodes[0].value.length;
+                                
+
+                                if(bodyTextLength) {
+
+                                    let bodyText = createBody.childNodes[0].value;
+                                    
+                                    console.log(bodyText);
+                                    
+                                    var output =  element.parentNode.insertBefore(createOutputBox,createMainEdtr);
+                                    
+                                    console.log(output)
+
+                                    output.innerHTML = bodyText;
+                                }
+
+                            }
+
+                        break;
+
+                    }
+                }
+                
+               
+
+            });
+
+            createFooter.classList.add("move-right")
+
             //write name here
             let boldBtn = this.createBtn("B");
             
             boldBtn.addEventListener("click",function() {
-                
-                let selectedText  = window.getSelection().toString();
+            
+                if( createBody.hasChildNodes() && window.getSelection().toString().length ) {
 
-                var selElm  = document.createElement("SPAN");
+                    let selectedText  = window.getSelection().toString();
 
-                selElm.textContent = selectedText;
-
-                if(createBody.hasChildNodes()) {
+                    var selElm  = document.createElement("SPAN");
+    
+                    selElm.textContent = selectedText;
 
                     console.log(createBody.childNodes[0].value.toString());
-
                     
-                }
+                    var str = createBody.childNodes[0].value.toString();
+                    createBody.childNodes[0].value = str.replace(selectedText,selectedText.bold());
 
-                //console.log(selectedText);
+                }
 
             });
 
             let italicBtn = this.createBtn("I");
+
+            italicBtn.addEventListener("click",function() {
+            
+                if( createBody.hasChildNodes() && window.getSelection().toString().length ) {
+
+                    let selectedText  = window.getSelection().toString();
+                    console.log(selectedText.length);
+                    var selElm  = document.createElement("SPAN");
+    
+                    selElm.textContent = selectedText;
+
+                    console.log(createBody.childNodes[0].value.toString());
+                    
+                    var str = createBody.childNodes[0].value.toString();
+                    createBody.childNodes[0].value = str.replace(selectedText,selectedText.italics());
+
+                }
+
+            }); 
+
             let underLineBtn = this.createBtn("U");
 
             createHeader.append(boldBtn,italicBtn,underLineBtn);
@@ -75,6 +149,7 @@ class TxtEditor {
             // createMainEdtr.parentNode.insertBefore(createFooter,createMainEdtr);
             
             var newNode = element.parentNode.insertBefore(createMainEdtr,element);
+           
             
            
 
